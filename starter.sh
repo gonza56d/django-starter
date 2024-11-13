5644500e5f531b9d7311ea9c3d7750603b8e17d9
gonza56d <gonza56d@gmail.com>
2024-11-12 22:35:43 -0300

    start project

--- starter.sh
+++ starter.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Generating a new Django project with THE PROPER layout."
+if [ -z "$1" ]; then
+    echo "Error: No argument provided for project name."
+    echo "Usage: $0 <project-name>"
+    exit 1
+fi
+
+echo " *** Generating a new Django project with THE PROPER layout."
+echo " Remember to have Django installed in your environment."
+echo " *** Project name: $1"
+# generate project
+django-admin startproject  $1 .
