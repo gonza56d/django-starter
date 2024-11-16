9d23097b35c491a3c9a1d5de8e8851bbde88af57
gonza56d <gonza56d@gmail.com>
2024-11-15 23:05:42 -0300

    remove dockerizing, lets go for python instead

--- django_starter.sh
+++ django_starter.sh
@@ -42,40 +42,4 @@ ALLOWED_HOSTS = []" >> $1/config/settings/production.py
 ## create env files
 touch .env
 touch .env.copy
-## Copy seret key and clean up $1/config/settings/base.py
-# Extract the value of SECRET_KEY
-SECRET_KEY=$(sed -n "s/^SECRET_KEY = '\(.*\)'$/\1/p" $1/config/settings/base.py)
-
-# Check for macOS and use pbcopy
-if command -v pbcopy &> /dev/null; then
-    echo -n "$SECRET_KEY" | pbcopy
-    pbpaste > .env
-    pbpaste > .env.copy
-# Check for Linux and use xclip or xsel
-elif command -v xclip &> /dev/null; then
-    echo -n "$SECRET_KEY" | xclip -selection clipboard
-    xclip -selection clipboard -o > .env
-    xclip -selection clipboard -o > .env.copy
-elif command -v xsel &> /dev/null; then
-    echo -n "$SECRET_KEY" | xsel --clipboard
-    xsel --clipboard --output > .env
-    xsel --clipboard --output > .env.copy
-else
-    echo "No clipboard tool found. Please install pbcopy and pbpaste (macOS) or xclip/xsel (Linux)."
-    exit 1
-fi
-
-
-sed -i "s/# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tov=m1&m)qjx&f-%ds^sip*gm%iu=5!yma9ekx4_b)r!b58#e1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-//g" $1/config/settings/base.py
-
-echo "Info: SECRET_KEY moved to your .env and .env.copy files."
 
